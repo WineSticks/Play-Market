20059c72379299422d6bd3d1312d04869d452bcd
ZED <inventoruzb@gmail.com>
2022-10-01 13:57:00 +0500

    UZBEK

---
+++ script.js
@@ -0,0 +1,49 @@
+/* var mir = +prompt("Son Kiriting")
+var mir1 = ""
+var mir2 = 0
+
+while(mir2 < mir){
+    mir1 = mir1 + "*"
+    document.write(mir1 + "<br>") 
+    mir2++
+}
+var mir111 = +prompt("Son 1")
+var mir11 = ""
+var mir22 = 0
+
+while(mir22 < mir111){
+    mir11 = mir11 + "*"
+    document.write(mir11 + "<br>") 
+    mir22++
+} 
+var mir1111 = +prompt("Son 2")
+var mir112 = ""
+var mir222 = 0 
+
+while(mir222 < mir1111){
+    mir112 = mir112 + "*"
+    document.write(mir112 + "<br>") 
+    mir222++
+}  */
+/* var num = prompt("Login")
+var res;
+
+if(num == 123){
+    res = "Togri topdingiz"
+}
+else if(num > 1234){
+    res = "To`gri login emas"
+}
+else if(num < 1234){
+    res = "To`gri login emas"
+}
+else{
+    res = "Yoqol"
+} 
+alert(res) */
+/* let  myName = "UzbUZB"
+
+myName = "UZB1"
+
+console.log(myName) */
+
